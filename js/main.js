52d1f04545609d2622df4cb65de8daf20400dbee
Hollenbeck <k_hollenbeck@ats-hr420-dm24.fc.ca>
2020-01-15 08:22:41 -0500

    netflix navbar

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'; 
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
 
-// set up a viewmodel and use the Vue framework
-const my_vm = (() => {
-    // variables, or define components, whatever
+var vm = new Vue({
+  el: "#app",
 
-    let vue_VM = new Vue({
-        data: {
-            message: "Hello from Vue!",
+  data: {
 
-            collection: [
-                {name: "Joe", role: "Prof"},
-                {name: "Trevor", role: "Prof"},
-                {name: "Keiran", role: "Student"}
-            ]
-        },
+    // mock up the user - this well eventually come from the database UMS (user management system)
 
-        methods: {
-            logClicked() {
-                console.log('clicked on an element!');
-            }
-        }
-    }).$mount("#app");
-})();
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+
+  }
+});
